8856e0023ce09c571b168836300da0d91fc40479
alison <luoxiaoqing44@gmail.com>
2026-04-01 16:31:42 +0800

    Complete 06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -2,5 +2,12 @@ const repeatString = function() {
 
 };
 
+function repeatString(string,num){
+    let result = "";
+    for (i = 0; i < num; i++){
+        result += string;
+    }
+    return result;
+}
 // Do not edit below this line
 module.exports = repeatString;
