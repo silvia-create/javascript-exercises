28d38694be71ed5ae001765faba432bacee93fb4
alison <luoxiaoqing44@gmail.com>
2026-04-02 17:11:19 +0800

    feat:add sumAll basic loop; pending a>b swap logic

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let total = 0;
+    if(!(a>0 && b>0 && a<b)){
+        alert("Please enter 2 positive integers");
+    }
+    for(let i = a; i <= b; i++){
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
